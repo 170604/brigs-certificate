13d6a4ec35c8dfdba891405f520c8f78b9a14291
170604 <133034635+170604@users.noreply.github.com>
2025-12-11 11:04:07 +0530

    Update3

--- script.js
+++ script.js
@@ -299,8 +299,8 @@ function initSalesQuizApp() {
 function initComplaintApp() {
   // ✅ Google Apps Script Webhook (public URL, not workspace-restricted)
   const COMPLAINT_GAS_WEBHOOK_URL =
-    "https://script.google.com/macros/s/AKfycbxAMgz78VaQI00yWeVvXYF0jXRIYRV7LPXQio2Tjcwo1l4ipT-cOigcHXnBSHqP84Ro/exec";
-
+    "https://script.google.com/macros/s/AKfycbyfbTIL4hqqx437Mq-vXwwxi7axaiyBmuo378HJhCNZ5RT_Cnp8QWyZeF4Wx5yIXV6A/exec";
+exec
   // === Element references ===
   const complaintIdInput = document.getElementById("complaintId");
   const complaintTitleInput = document.getElementById("complaintTitle");
